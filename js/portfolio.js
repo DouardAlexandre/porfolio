1f7d8823ca65595810c8100d0037a6c9b4acd84d
alexandre <kalambik@gmail.com>
2017-11-06 12:33:28 +0100

    ascii+merge

--- js/portfolio.js
+++ js/portfolio.js
@@ -23,53 +23,8 @@
      selector: ".project__card"
     });
 
-    /*//Lightbox
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-       event.preventDefault();
-       $(this).ekkoLightbox({
-                alwaysShowClose: true
-                
-            });
-    });*/
-   /* $(document.body).on('show.bs.modal', function () {
-$(window.document).find('html').addClass('modal-open');
-});
-$(document.body).on('hide.bs.modal', function () {
-$(window.document).find('html').removeClass('modal-open');
-});*/
-    //gallery lightbox
-    $('.thumbnail').click(function(){
-          $('.modal-body-g').empty();
-          var title = $(this).parent('a').attr("title");
-          $('.modal-title-g').html(title);
-          $($(this).parents('div').html()).appendTo('.modal-body-g');
-          $('#myModal').modal({show:true});
-       //   $(document.body).removeClass('modal-open');
-       // $("body").css('overflow-y', 'hidden');
-        //$("#myModal").css('overflow-y', 'hidden');
-    });
-   /* $(document.body).on('show.bs.modal', function () {
-      $(document.body).addClass('modal-open');
-    });
-    $(document.body).on('hide.bs.modal', function () {
-      $(document.body).removeClass('modal-open');
-    });*/
-   $("#close").click(function(){
- 
-        $("#myModal").modal("hide");
-        //$("body").css('overflow-y', 'hidden');
-    });
-    /*$(".close-modal").click(function(){
-        $("body").css('overflow-y', 'hidden');
-    });*/
-   /* $(".portfolio-modal").click(function(){
-        $("body").css('overflow', 'hidden');
-    });*/
+     $('[data-toggle="tooltip"]').tooltip()
     
-    /*jQuery('.closeit').click(function () {
-         jQuery('body').removeClass('modal-open');
-    });*/
-
     //animation bird
     var morphing = anime({
       targets: '.bird .polymorph path',
@@ -124,21 +79,6 @@ $(window.document).find('html').removeClass('modal-open');
       loop: true
     });
 
-   /* var morphing2 = anime({
-      targets: '#morphing2 .polymorph2 path',
-          d: [
-              { value: "M216.666,178c0,0,29.5-23,81-9c0,0,22-19.5,51-3.5c0,0-55-8.5-37.5,38 C306.938,203.886,295.666,156,216.666,178z" },
-              { value: "M216.666,178c0,0,29.5-23,81-9c0,0,22-19.5,51-3.5c0,0-55-8.5-37.5,38 C306.938,203.886,295.666,156,216.666,178z" },
-              { value: "M251.5,221.5c0,0-3.5-67.5,48-53.5c0,0,23.5,3,43,27.5c0,0-56-44.5-30,9 C308.272,204.886,278,140.5,251.5,221.5z" },
-              { value: "M242.107,192.6c0,0,65.559-9.698,69.559-13.535c0,0,5.897,4.574,21,9.668c0,0-21-11.203-11.063-25.136 C321.603,163.597,300.604,182.242,242.107,192.6z" },
-              { value: "M245.107,155.6c0,0,65.559,9.362,69.559,13.066c0,0,5.897-4.415,21-9.333c0,0-21,10.814-11.063,24.264 C324.603,183.597,303.604,165.599,245.107,155.6z" }
-          ],
-      easing: 'easeOutQuad',
-      duration: 600,
-      loop: true
-  });*/
- 
-
 
     "use strict"; // Start of use strict
 
@@ -180,6 +120,38 @@ $(window.document).find('html').removeClass('modal-open');
         });
     });
 
+console.log('%c Bienvenue! ', 'background: #222; color: #bada55',
+            'more text');
 
-
+console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$_$$$$$$$$$$$$$$$$_$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$__$$$$$$$$$$$$$$_$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$$_______________$$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$___________________$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$____$$$_________$$$____$$$$$$$$$$$$$\
+$$$$$$$$$$$$$_____$$$_________$$$_____$$$$$$$$$$$$\
+$$$$$$$$$$$$___________________________$$$$$$$$$$$\
+$$$$$$$$$$$$___________________________$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\
+$$$$$____$$$____________________________$$$____$$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$______$$____________________________$$______$$\
+$$$$$____$$$____________________________$$$____$$$\
+$$$$$$$$$$$$____________________________$$$$$$$$$$\
+$$$$$$$$$$$$____________________________$$$$$$$$$$\
+$$$$$$$$$$$$___________________________$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$______$$$$$$_____$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$______$$$$$$_____$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$______$$$$$$_____$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$______$$$$$$_____$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$______$$$$$$_____$$$$$$$$$$$$$$$$\
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\
+');
 })(jQuery); // End of use strict
